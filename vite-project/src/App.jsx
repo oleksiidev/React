df074c8d89079b4ee46866fd38f1149a0fe5c516
Oleksii <oleksii.sydorenko.dev@gmail.com>
2023-05-03 22:01:10 +0200

    NewTodoForm

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from 'react'
 import './style.css'
+import { NewTodoForm } from './NewTodoForm';
 
 export default function App() {
-  const [newItem, setNewItem] = useState("");
+  
   const [todos, setTodos] = useState([]);
 
-  function handleSubmit(e) {
-    e.preventDefault()
-
-    setTodos(currentTodos => {
+  function addTodo(title){
+     setTodos(currentTodos => {
       return [...currentTodos, {
-        id: crypto.randomUUID(),
-        title: newItem,
-        completed: false
+          id: crypto.randomUUID(),
+          title,
+          completed: false
       },]
-    })
-
-    setNewItem("")
-
+  })
   }
 
   function toogleTodo(id, completed) {
@@ -32,21 +28,18 @@ export default function App() {
     })
   }
 
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <div>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New item</label>
-          <input
-            value={newItem}
-            onChange={e => setNewItem(e.target.value)}
-            type="text"
-            id="item" />
-        </div>
-        <button className='btn'>Add</button>
-      </form>
+      <NewTodoForm onSubmit={addTodo}/>
       <h1 className='header'>Todo List</h1>
       <ul className='list'>
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return <li key={todo.id}>
             <label >
@@ -54,7 +47,8 @@ export default function App() {
                 onChange={e => toogleTodo(todo.id, e.target.checked)} />
               {todo.title}
             </label>
-            <button className='btn btn-danger'>Delete</button>
+            <button onClick={() => deleteTodo(todo.id)} 
+            className='btn btn-danger'>Delete</button>
           </li>
         })}
 
